ad7da1be9ec92e9082dee2dce9a2fc475da2267e
berenzorn <berenzorn@gmail.com>
2020-04-17 14:34:41 +0300

    cheap ops with new in diff

--- comparator.py
+++ comparator.py
@@ -1,6 +1,7 @@
 import argparse
 import pandas as pd
 from table import Table
+import datetime
 
 
 class C9(Table):
@@ -8,8 +9,6 @@ class C9(Table):
         sid = ""
         for i in row:
             sid = f"{sid}, '{str(i).rstrip()}'"
-        # sid = f"'{row[4]}{row[3]}{row[5]}', {string[3:]}"
-        # print(sid[2:])
         insert = f"INSERT INTO {self.name} (id, country, network, tadig, mcc, mnoid, profile, ws_price, ws_inc, " \
             f"retail_price, rp_inc, 4g, blocking, cheapest) VALUES ({sid[2:]});"
         self.table_execute(insert)
@@ -26,9 +25,9 @@ class C9(Table):
         self.table_execute(f"UPDATE {self.name} AS C CROSS JOIN (SELECT {prev.name}.id, {prev.name}.cheapest, {self.name}.cheapest AS MARK "
                            f"FROM {prev.name} CROSS JOIN {self.name} on {prev.name}.id = {self.name}.id WHERE {prev.name}.cheapest IS NULL "
                            f"AND {self.name}.cheapest IS NOT NULL) AS F USING (id) SET C.differ = '1' WHERE F.MARK IS NOT NULL;")
-        # self.table_execute(f"ALTER TABLE {self.name} ADD COLUMN `cheapest_prev` INT NULL AFTER `blocking`;")
-        # self.table_execute(f"UPDATE {self.name} SET `cheapest_prev` = (SELECT `cheapest` FROM {prev.name} WHERE {self.name}id = {prev.name}.id);")
-        self.cnx.commit()
+        self.table_execute(f"UPDATE {self.name} AS C CROSS JOIN (SELECT {prev.name}.id, {prev.name}.cheapest as MARK, {self.name}.cheapest "
+                           f"FROM {prev.name} CROSS JOIN {self.name} ON {prev.name}.id = {self.name}.id WHERE {prev.name}.cheapest IS NOT NULL "
+                           f"AND {self.name}.cheapest IS NULL) AS F USING (id) SET C.differ = '0' WHERE F.MARK IS NOT NULL;")
 
     def show_new_ops(self, prev):
         self.table_execute(f"SELECT id FROM {self.name};")
@@ -40,6 +39,7 @@ class C9(Table):
         self.table_execute(f"ALTER TABLE {self.name} ADD COLUMN `new_ops` VARCHAR(10) NULL;")
         for i in added_ops:
             self.table_execute(f"UPDATE {self.name} SET {self.name}.new_ops = 'new' WHERE id = '{i[0]}';")
+        self.table_execute(f"UPDATE {self.name} SET {self.name}.differ = '1' WHERE cheapest IS NOT NULL AND {self.name}.new_ops = 'new';")
         # for i in removed_ops:
         #     self.table_execute(f"INSERT INTO {self.name} ")
 
@@ -52,16 +52,13 @@ class C9(Table):
             mnc = x[0].split('/')[1]
             if x[1] is None:
                 plmn_list.append(f"{mcc},{mnc},3G")
-                # print(f"{mcc},{mnc},3G")
             else:
                 plmn_list.append(f"{mcc},{mnc},4G")
-                # print(f"{mcc},{mnc},4G")
         return plmn_list
 
 
 class Common(Table):
     def table_combine(self, prev, curr) -> list:
-        # self.table_execute(f"SELECT * FROM {name};")
         self.table_execute(f"SELECT {curr}.id, {curr}.country, {curr}.network, {curr}.tadig, {curr}.mcc, {curr}.mnoid, {curr}.profile, {prev}.ws_price, "
                            f"{curr}.ws_price, {prev}.retail_price, {curr}.retail_price, {curr}.4g, {curr}.blocking, {prev}.cheapest, {curr}.cheapest, "
                            f"{curr}.differ, {curr}.new_ops from {curr} LEFT OUTER JOIN {prev} on {curr}.id = {prev}.id;")
@@ -82,9 +79,12 @@ def read_excel(filename, skiprows) -> list:
 
 
 def write_excel(combined: list, filename: str):
+    parts = filename.split('.')
+    filename = f"{parts[0]}-{datetime.datetime.now().strftime('%d.%m.%y-%H.%M')}.{parts[1]}"
     df = pd.DataFrame(combined,
-                      columns=['id', 'country', 'network', 'tadig', 'mcc', 'mnoid', 'profile', 'ws_price_prev', 'ws_price_curr',
-                               'retail_price_prev', 'retail_price_curr', '4g', 'blocking', 'cheapest_prev', 'cheapest_curr', 'differ', 'new_ops'])
+                      columns=['id', 'country', 'network', 'tadig', 'mcc', 'mnoid', 'profile',
+                               'ws_price_prev', 'ws_price_curr', 'retail_price_prev', 'retail_price_curr',
+                               '4g', 'blocking', 'cheapest_prev', 'cheapest_curr', 'differ', 'new_ops'])
     df.to_excel(filename)
 
 
@@ -122,9 +122,9 @@ if __name__ == '__main__':
     fill_table(c9_update, args.update, 1)
     c9_update.show_difference(c9_table)
     c9_update.show_new_ops(c9_table)
-    write_csv(c9_update.plmn_make('G'), 'plmn_G.txt')
-    write_csv(c9_update.plmn_make('G+'), 'plmn_Gplus.txt')
-    write_csv(c9_update.plmn_make('UL'), 'plmn_UL.txt')
+    # write_csv(c9_update.plmn_make('G'), 'plmn_G.txt')
+    # write_csv(c9_update.plmn_make('G+'), 'plmn_Gplus.txt')
+    # write_csv(c9_update.plmn_make('UL'), 'plmn_UL.txt')
     cmn_table = Common(name=str(args.output), creds=creds)
     combined = cmn_table.table_combine(c9_table.name, c9_update.name)
     write_excel(combined, args.output)
